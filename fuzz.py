dee053901d4131e59494bf685885c8506e857acf
Tiffany W <jtw0085@auburn.edu>
2022-12-01 12:46:14 -0600

    comma

--- fuzz.py
+++ fuzz.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
                 (0.0, 0.0),
                 (None, 'ht.lm')
             ]
-        )
+        ),
         (
             generateAttack, [
                 (None, None),
